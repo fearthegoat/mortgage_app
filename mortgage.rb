f29ee6862a9b49d0af53274cb34d59a7db6f57f1
Kevin Manuel <kmanuel09@gmail.com>
2015-08-21 08:39:28 -0400

    completes basic mortgage

--- mortgage.rb
+++ mortgage.rb
@@ -1,24 +1,36 @@
 class Mortgage
 
-  attr_accessor :principle
+  attr_reader :beginning_principle
+  attr_accessor :payments_made
   attr_accessor :interest
   attr_accessor :payment
+  attr_accessor :remaining_principle
 
-  def initialize(interest_rate, principle)
-    @interest = interest_rate
-    @principle = principle
-    @payment = 2000
+  def initialize(interest_rate, principle, term_in_years)
+    @interest = interest_rate/100
+    @beginning_principle = principle
+    @remaining_principle = principle
+    @payment = ((@interest/12)*(@beginning_principle) / (1 - (1 + (@interest/12))**(-term_in_years*12))).round(2) # annuity equation
+    @interest_paid = 0.0
+    @payments_made = 0
   end
 
   def interest_payment
-    @principle * monthly_interest
+    (@remaining_principle * monthly_interest).round(2)
   end
 
   def principle_payment
-    @payment - interest_payment
+    (@payment - interest_payment).round(2)
   end
 
   def monthly_interest
     @interest / 12
   end
+
+  def make_payment
+    @interest_paid += interest_payment
+    @remaining_principle -= principle_payment
+    @payments_made += 1
+  end
 end
+
