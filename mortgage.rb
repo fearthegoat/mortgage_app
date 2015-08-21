d580455b938b7f384a30f3cdd0cd0f96c6f96004
Kevin Manuel <kmanuel09@gmail.com>
2015-08-20 23:09:04 -0400

    adds interest and principle payment

--- mortgage.rb
+++ mortgage.rb
@@ -1,9 +1,21 @@
 class Mortgage
-  def initialize
+
+  attr_accessor :principle
+  attr_accessor :interest
+  attr_accessor :payment
+
+  def initialize(interest_rate, principle)
+    @interest = interest_rate
+    @principle = principle
+    @payment = 2000
   end
 
-  def interest_payment(principle)
-    principle * monthly_interest
+  def interest_payment
+    @principle * monthly_interest
+  end
+
+  def principle_payment
+    @payment - interest_payment
   end
 
   def monthly_interest
