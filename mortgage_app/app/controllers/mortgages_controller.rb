941ff564f068c55e5374bfc190fb94f20dee8a24
Kevin Manuel <kmanuel09@gmail.com>
2015-09-03 23:28:04 -0400

    adds some logic on the controller

--- mortgage_app/app/controllers/mortgages_controller.rb
+++ mortgage_app/app/controllers/mortgages_controller.rb
@@ -4,6 +4,19 @@ class MortgagesController < ApplicationController
   end
 
   def create
+    mortgages = []
+    params[:mortgage][:rate].each_with_index do |rate, index|
+      mortgage_new = []
+      mortgage_new << params[:mortgage][:loan].to_d
+      mortgage_new << rate.to_d
+      mortgage_new << params[:mortgage][:term][index].to_i
+      mortgages << mortgage_new
+    end
+    m = []
+    mortgages.each do |loan, rate, term|
+      mortgage_new = Mortgage.new(rate, loan, term)
+      m << mortgage_new.payment
+    end
     raise :oops
     # render 'create.js'
   end
