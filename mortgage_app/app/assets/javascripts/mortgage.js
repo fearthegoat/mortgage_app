4c24c8deff61560a78ff66d4374315e2b330bcc2
Kevin <kmanuel09@gmail.com>
2015-10-24 00:04:06 -0400

    adds some mobile site corrections

--- mortgage_app/app/assets/javascripts/mortgage.js
+++ mortgage_app/app/assets/javascripts/mortgage.js
@@ -13,11 +13,14 @@ $(document).ready(function() {
     if (x < max_fields) {
       x++;
       $(wrapper).append('<div class="row"> \
-        <div class="input-field col m12 l3"> \
+        <div class="hide-on-med-and-up center-align"> \
+          <h5>Fixed Rate Mortgage</h5> \
+        </div> \
+        <div class="input-field col s12 l3"> \
         <input id="fixed_rate" class="validate" placeholder="i.e: 3.75" step=".125" min="1" max="15" type="number" name="mortgage[rate][]"> \
         <label for="fixed_rate" class="active">Interest Rate (APR)</label> \
         </div> \
-        <div class="input-field col m12 l3 offset-l2"> \
+        <div class="input-field col s12 l3 offset-l2"> \
         <input id="fixed_term" class="validate" placeholder="i.e.: 30" step="5" min="5" max="30" type="number" name="mortgage[term][]"> \
         <label for="fixed_term" class="active">Term (years)</label> \
         </div> \
@@ -45,27 +48,30 @@ $(document).ready(function() {
     if (x < max_fields) {
       x++;
       $(wrapper).append('<div class="row"> \
-        <div class="input-field col m12 l2"> \
+        <div class="hide-on-med-and-up center-align"> \
+          <h5>Adjustable Rate Mortgage</h5> \
+        </div> \
+        <div class="input-field col s12 l2"> \
         <input id="adjustable_rate" class="validate" placeholder="i.e: 3.75" step=".125" min="1" max="15" type="number" name="mortgage[rate][]"> \
         <label for="adjustable_rate" class="active">Interest Rate (APR)</label> \
         </div> \
-        <div class="input-field col m12 l2"> \
+        <div class="input-field col s12 l2"> \
         <input id="adjustable_term" class="validate" placeholder="i.e.: 30" step="5" min="5" max="30" type="number" name="mortgage[term][]"> \
         <label for="adjustable_term" class="active">Term (years)</label> \
         </div> \
-        <div class="input-field col m12 l2"> \
+        <div class="input-field col s12 l2"> \
         <input id="years_before_first_adjustment" class="validate" placeholder="i.e.: 5" step="1" min="1" max="30" type="number" name="mortgage[years_before_first_adjustment][]"> \
         <label for="years_before_adjustment" class="active">Years Before 1st Adjustment</label> \
         </div> \
-        <div class="input-field col m12 l2"> \
+        <div class="input-field col s12 l2"> \
         <input id="max_rate_adjustment" class="validate" placeholder="i.e.: 2" step=".125" min="1" max="10" type="number" name="mortgage[max_rate_adjustment_period][]"> \
         <label for="max_rate_adjustment" class="active">Max Rate Adjustment, Each Period</label> \
         </div> \
-        <div class="input-field col m12 l2"> \
+        <div class="input-field col s12 l2"> \
         <input id="max_rate_adjustment_term" class="validate" placeholder="i.e.: 3" step=".125" min="1" max="10" type="number" name="mortgage[max_rate_adjustment_term][]"> \
         <label for="max_rate_adjustment_term" class="active">Max Rate Adjustment for Term</label> \
         </div> \
-        <div class="input-field col m12 l2"> \
+        <div class="input-field col s12 l2"> \
         <input id="years_between_adjustments" class="validate" placeholder="i.e.: 1" step="1" min="1" max="15" type="number" name="mortgage[years_between_adjustments][]"> \
         <label for="years_between_adjustments" class="active">Years Between Rate Adjustments</label> \
         </div> \
