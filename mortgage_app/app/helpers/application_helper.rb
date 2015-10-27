e256901bea0318e2d5c125ef0c7a4ba09fde7c26
Kevin <kmanuel09@gmail.com>
2015-10-26 23:13:42 -0400

    adds title

--- mortgage_app/app/helpers/application_helper.rb
+++ mortgage_app/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+def title(title = nil)
+  if title.present?
+    content_for :title, title
+  else
+    content_for?(:title) ? "ARM Calculator" + ' | ' + content_for(:title) : "ARM Calculator"
+  end
+end
+
 end
